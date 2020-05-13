068e5b6f0f85c37b9c61a37b84c5c43c88f56e23
Martin Sanne Kristiansen <martin@heydays.no>
2020-05-13 09:48:00 +0200

    :bug:

--- functions/oembed.js
+++ functions/oembed.js
@@ -1,54 +1,25 @@
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context, callback) => {
   const { extract } = require('oembed-parser')
-  const url =
-    event.queryStringParameters.url ||
-    'https://www.youtube.com/watch?v=8jPQjjsBbIc'
+  const url = 'https://www.youtube.com/watch?v=8jPQjjsBbIc'
 
-  extract(url)
-    .then((data) => {
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({
-          result: data,
-          status: 'success',
-        }),
-      })
-    })
-    .catch((err) => {
-      callback(null, {
-        statusCode: 500,
-        body: JSON.stringify({
-          error: err,
-          status: 'error',
-        }),
-      })
-    })
+  try {
+    const oembed = await extract(url)
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        requested: url,
+        status: 'success',
+        data: oembed,
+      }),
+    }
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        requested: url,
+        status: 'error',
+        data: err,
+      }),
+    }
+  }
 }
-
-// export const handler = (event, context, callback) => {
-//   const { extract } = require('oembed-parser')
-//   // const { url } = JSON.parse(event.body)
-//   const url =
-//     event.queryStringParameters.url ||
-//     'https://www.youtube.com/watch?v=8jPQjjsBbIc'
-
-//   extract(url)
-//     .then((data) => {
-//       callback(null, {
-//         statusCode: 200,
-//         body: JSON.stringify({
-//           result: data,
-//           status: 'success',
-//         }),
-//       })
-//     })
-//     .catch((err) => {
-//       callback(null, {
-//         statusCode: 500,
-//         body: JSON.stringify({
-//           error: err,
-//           status: 'error',
-//         }),
-//       })
-//     })
-// }
