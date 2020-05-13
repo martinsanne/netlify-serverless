9a8a37811f79c91e19f928c694b1b07cd9d2b425
Martin Sanne Kristiansen <martin@heydays.no>
2020-05-13 16:50:32 +0200

    test

--- functions/oembed.js
+++ functions/oembed.js
@@ -1,18 +1,42 @@
-const { extract } = require('../src/oembed')
+// const { extract } = require('../src/oembed')
 
-exports.handler = (event, context, callback) => {
-  const URL = 'https://vimeo.com/412071250'
-  extract(URL)
-    .then((res) => {
+// exports.handler = (event, context, callback) => {
+//   const URL = event.queryStringParameters.url
+//   extract(URL)
+//     .then((res) => {
+//       callback(null, {
+//         statusCode: 200,
+//         body: JSON.stringify(res),
+//       })
+//     })
+//     .catch((err) => {
+//       callback(null, {
+//         statusCode: 500,
+//         body: JSON.stringify(err),
+//       })
+//     })
+// }
+export const handler = (event, context, callback) => {
+  const { extract } = require('oembed-parser')
+  const { url } = JSON.parse(event.body)
+
+  extract(url)
+    .then((data) => {
       callback(null, {
         statusCode: 200,
-        body: JSON.stringify(res),
+        body: JSON.stringify({
+          result: data,
+          status: 'success',
+        }),
       })
     })
     .catch((err) => {
       callback(null, {
         statusCode: 500,
-        body: 'Hello, Hell',
+        body: JSON.stringify({
+          error: err,
+          status: 'error',
+        }),
       })
     })
 }
